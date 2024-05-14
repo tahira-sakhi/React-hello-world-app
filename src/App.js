c2dd483b39eabe8f0163b8ea5ae70cf2731d63f2
tahira-sakhi <tahi.parveen93@gmail.com>
2024-05-15 00:36:13 +0500

    added content

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO WORLD!
-        </p>
+        <p>HELLO WORLD!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
+          This is my React app!
+          <br />
           Learn React
         </a>
       </header>
